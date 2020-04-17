0f19e45cea2ac53e470bab79cf13f62915d4b3c7
huesca12 <thebritishspy12@gmail.com>
2020-04-17 17:38:21 +0100

    potato

--- eulercalc/urls.py
+++ eulercalc/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('terminal/', include('terminal.urls')),
-    path('', include('terminal.urls'))
+    path('potato/', include('terminal.urls')),
+    path('', include('terminal.urls')),
 ]
