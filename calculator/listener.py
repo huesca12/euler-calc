208ca60977ae528a26b234cbb7da6710eea9fdf8
huesca12 <thebritishspy12@gmail.com>
2020-05-03 17:21:06 +0100

    specified collection

--- calculator/listener.py
+++ calculator/listener.py
@@ -3,7 +3,7 @@ import pymongo
 from bson.json_util import dumps
 
 client = pymongo.MongoClient(str(os.environ.get('MONGODB_URL')))
-change_stream = client.changestream.collection.watch()
+change_stream = client.terminal_rawrequest.watch()
 for change in change_stream:
     print(dumps(change))
     print('') # for readability only
