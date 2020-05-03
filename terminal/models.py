a9f87770d27d4f475524b55f7ed2680786879c80
huesca12 <thebritishspy12@gmail.com>
2020-05-03 15:39:04 +0100

    removed identifier

--- terminal/models.py
+++ terminal/models.py
@@ -3,4 +3,3 @@ from django.db import models
 # Create your models here.
 class RawRequest(models.Model):
     content = models.CharField(max_length=130)
-    identifier = models.CharField(max_length=30)
