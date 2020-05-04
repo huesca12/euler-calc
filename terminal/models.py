2b441b632d4bab6374b7a4631cf61362c2a79b07
Nicolas Casey <nickcaseycipres@gmail.com>
2020-05-04 08:31:08 +0100

    added UUID

--- terminal/models.py
+++ terminal/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class RawRequest(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     content = models.CharField(max_length=130)
